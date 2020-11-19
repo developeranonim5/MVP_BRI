1033ccf92f1e7125b49343a61880780a3c9b3e7d
nailaiffah11 <nailaiffah11@gmail.com>
2020-11-19 13:35:35 +0700

    menambahkan phone number, menguba type jadi number

--- src/components/customer/DataCustomer.jsx
+++ src/components/customer/DataCustomer.jsx
@@ -61,9 +61,11 @@ const DataCustomer =()=>{
 
   <Form.Control type="text" placeholder="Full Name" />
   <br />
-  <Form.Control type="text" placeholder="ID KTP" />
+  <Form.Control type="number" placeholder="ID KTP" />
   <br />
-  <Form.Control type="text" placeholder="No Rekening" />
+  <Form.Control type="number" placeholder="No Rekening"  />
+  <br />
+  <Form.Control type="tel" placeholder="Phone Number"  />
   <br />
   <Button  variant="secondary">Save</Button>
 
